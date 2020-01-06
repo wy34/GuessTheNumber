eeb478d30dfc0b9c3209cf62b4707bbc7e4fd670
William Yeung <williamyeung1141998@gmail.com>
2020-01-06 15:37:00 -0500

    Called the 'show' functions inside the displayResult function

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
  * DONE: Console whether the guess is too high, too low, or is correct inside playGame function
  * DONE: Create a function called displayResult to move the logic for if the guess is too high, too low, or correct
  * DONE: Complete the showYouWon, showNumberAbove, showNumberBelow
- * TODO: Use the showYouWon... functions within displayResult to display the correct dialog
+ * DONE: Use the showYouWon... functions within displayResult to display the correct dialog
  * TODO: Save the guess history in a variable called guess
  * TODO: Display the guess history using displayHistory() function
  * TODO: Use the initGame() function to restart the game
@@ -37,11 +37,11 @@ function playGame(){
 // *CODE GOES BELOW HERE *
 function displayResult(numberGuess) {
   if (parseInt(numberGuess) === correctNumber) {
-    console.log("correct");
+    showYouWon();
   } else if (parseInt(numberGuess) > correctNumber) {
-    console.log("too high")
+    showNumberAbove();
   } else {
-    console.log("too low")
+    showNumberBelow();
   }
 }
 
